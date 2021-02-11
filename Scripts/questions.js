a04f3bd2031be93efa31c1042cde2da3eb002a19
Nick Boutin <nboutin109@gmail.com>
2021-02-10 19:00:23 -0500

    polishing

---
+++ Scripts/questions.js
@@ -0,0 +1,51 @@
+var questions = [{
+    Question: "If a function is a darker yellow color than normal in VScode what does it mean?",
+    Answers: {
+        A: 'It contains an error',
+        B: 'It is not being called',
+        C: 'It is on vacation',
+        D: 'It is formatted incorrectly'
+    },
+    correct: 'B'
+},
+{
+    Question: "What is jQuery?",
+    Answers: {
+        A: 'An alternative to Javascript',
+        B: 'A Javascript framework',
+        C: 'A social media platform',
+        D: 'The entrance to the Matrix'
+    },
+    correct: 'B'
+},
+{
+    Question: "What are the words True and False in Javascript?",
+    Answers: {
+        A: 'Strings',
+        B: 'Boolean values',
+        C: 'Both A and C',
+        D: 'Arrays'
+    },
+    correct: 'C'
+},
+{
+    Question: "Question",
+    Answers: {
+        A: "Answer",
+        B: "Answer",
+        C: "Answer",
+        D: "Answer"
+    },
+    correct: 'D'
+},
+{
+    Question: "How do you round an integer using Javascript?",
+    Answers: {
+        A: 'math.round(7.4)',
+        B: 'round(7.4)',
+        C: 'math.round = "7.4"',
+        D: 'You cannot round with Javascript'
+    },
+    correct: 'A'
+},
+]
---
+++ Scripts/script.js
@@ -0,0 +1,76 @@
+var index = 0;
+var time = 100;
+var timer;
+var score = 0;
+
+function render() {
+    $('.first').hide();
+    $('#second').removeClass('invisible');
+    time = questions.length * 20
+    timer = setInterval(clockTick, 1000);
+    $("#time").html("Your remaining time is: " + time);
+}
+
+function clockTick() {
+    time--;
+    $("#time").html("Your remaining time is: " + time)
+    if (time <= 0) {
+        end();
+    }
+}
+
+function setQuestion() {
+    $("#questionDiv").html(questions[index].Question);
+    $("#A1").html(questions[index].Answers.A);
+    $("#A2").html(questions[index].Answers.B);
+    $("#A3").html(questions[index].Answers.C);
+    $("#A4").html(questions[index].Answers.D);
+}
+
+function checking() {
+    if ($("input:radio[id=" + questions[index].correct + "]:checked").val() == "on") {
+        score++;
+        $('#correctness').html("That was correct!")
+        $('#scoreDiv').html("Your score is now: " + score)
+    } else {
+        $('#correctness').html("Oh no! That was incorrect!")
+        $('#scoreDiv').html("Your score is now: " + score)
+        time -= 20;
+    }
+}
+
+function end() {
+    $('#second').addClass('invisible')
+    $('#third').removeClass('invisible')
+    $('#resultsDiv').html("Congrats! Your final score is: " + score)
+}
+
+$('#start').click(function () {
+    render();
+    setQuestion();
+})
+
+$('#nextQuestion').click(function () {
+    if (($("input:radio[name='choices']:checked").val() !== "on")) {
+        $("#errorDiv").html("Please select an answer");
+        return;
+    }
+    else { $("#errorDiv").html("") }
+    if (time <= 0 || index == 4) {
+        end();
+        return;
+    }
+    checking();
+    index++;
+    setQuestion();
+})
+
+$('#tryAgain').click(function () {
+    index = 0;
+    score = 0;
+    $('#third').addClass('invisible');
+    $('#correctness').html('')
+    $('#scoreDiv').html('')
+    render();
+    setQuestion();
+})
